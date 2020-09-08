5d79d5e59af01355d34e416a8686ddff92731689
Hieunm <hieunguyenmanh13.cdc@gmail.com>
2020-09-08 14:04:26 +0700

    format

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const bodyParser = require('body-parser');
 const sequelize = require('./sequelize');
 const userRoutes = require('./routers/user.route')
 async function assertDatabaseConnectionOk() {
-	console.log(`Checking database connection...`);
-	try {
-		await sequelize.authenticate();
-		console.log('Database connection OK!');
-	} catch (error) {
-		console.log('Unable to connect to the database:');
-		console.log(error.message);
-		process.exit(1);
-	}
+  console.log(`Checking database connection...`);
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection OK!');
+  } catch (error) {
+    console.log('Unable to connect to the database:');
+    console.log(error.message);
+    process.exit(1);
+  }
 }
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -21,13 +21,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', userRoutes)
 
 async function init() {
-	await assertDatabaseConnectionOk();
+  await assertDatabaseConnectionOk();
 
-	console.log(`Starting Sequelize + Express example on port ${PORT}...`);
+  console.log(`Starting Sequelize + Express example on port ${PORT}...`);
 
-	app.listen(PORT, function () {
-		console.log(`Express server started on port ${PORT}. Try some routes, such as '/api/users'.`);
-	});
+  app.listen(PORT, function () {
+    console.log(`Express server started on port ${PORT}. Try some routes, such as '/api/users'.`);
+  });
 }
 
 init();
--- routers/user.route.js
+++ routers/user.route.js
@@ -4,40 +4,40 @@ const { models } = require('../sequelize');
 const auth = require('../middleware/auth')
 const router = express.Router()
 router.post('/users', async function (req, res) { // dang ky cho user
-    try {
+  try {
 
-        const user = await models.users.create(req.body)
-        //  console.log(req.body)
-        res.status(201).send({ user })
-    } catch (error) {
-        res.status(400).send(error)
-    }
+    const user = await models.users.create(req.body)
+    //  console.log(req.body)
+    res.status(201).send({ user })
+  } catch (error) {
+    res.status(400).send(error)
+  }
 })
 router.post('/users/login', async function (req, res) {
-    try {
-        const { email, password } = req.body
-        const user = await models.users.findByCredentials(email, password)
+  try {
+    const { email, password } = req.body
+    const user = await models.users.findByCredentials(email, password)
 
-        if (!user) {
+    if (!user) {
 
-            return res.sendstatus(401).send({ error: 'login failed! Check authentication credentials' })
-        }
-        const token = await user.generateAuthToken()
-        res.send({ user, token })
-    } catch (error) {
-        res.status(400).send(error)
+      return res.sendstatus(401).send({ error: 'login failed! Check authentication credentials' })
     }
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
+  } catch (error) {
+    res.status(400).send(error)
+  }
 })
 router.get('/users/me', auth, async function (req, res) { //lay du lieu user hien tai
-    res.send(req.user)
+  res.send(req.user)
 })
 router.post('/users/me/logout', auth, async function (req, res) {
-    try {
-        req.user.token = "",
-            await req.user.save()
-        res.send()
-    } catch (error) {
-        res.status(500).send(error)
-    }
+  try {
+    req.user.token = "",
+      await req.user.save()
+    res.send()
+  } catch (error) {
+    res.status(500).send(error)
+  }
 })
 module.exports = router
